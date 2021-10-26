6b29513105ff0d15f2fe19566bf6849b2458c14e
dreaith <noel80@hotmail.ca>
2021-10-26 14:53:15 -0400

    css+java

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,18 @@
+//= link_tree ../images
+//= link_tree ../fonts
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+
+//= link essentials.css
+//= link header.css
+//= link layout.css
+//= link quote-form.css
+//= link blue.css
+
+//= link contact.js
+//= link demo.revolution_slider.js
+//= link request-quote.js
+//= link scripts.js
+//= link font-icons.woff
+//= link fontawesome-webfont.woff2
